b1f3d1f1217420a419037535517910dd2222c0d6
sanatankumar <sanatan.iit@gmail.com>
2017-11-14 17:44:47 +0530

    updated start button logix

--- src/components/StartButton.js
+++ src/components/StartButton.js
@@ -2,10 +2,17 @@ import React, { Component } from 'react'
 import isPressed from './isPressedHOC'
 
 class StartButton extends Component {
+  state = {
+    isEverClicked: false,
+  }
+
   render() {
     console.log(this.props)
     return (
-      <button onClick={this.props.onClick}>Start</button>
+      <button onClick={() => {
+        !this.state.isEverClicked && this.setState({isEverClicked: true})
+        this.props.onClick()
+      }}>{this.state.isEverClicked ? 'Restart' : 'Start'}</button>
     )
   }
 }
